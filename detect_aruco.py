7396d956109d97f2b075b1ae68ca34cc51659447
tkC0ding <tkjagani@gmail.com>
2024-09-21 14:30:03 +0530

    transformed the rotation matrix into a quaternion

--- detect_aruco.py
+++ detect_aruco.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
@@ -15,6 +16,13 @@ detector = cv2.aruco.ArucoDetector(dictonary, detector_params)
 
 aruco_side_length = 0.14 #m
 
+object_points = np.array([
+    [-aruco_side_length/2, aruco_side_length/2, 0],
+    [aruco_side_length/2, aruco_side_length/2, 0],
+    [aruco_side_length/2, -aruco_side_length/2, 0],
+    [-aruco_side_length/2, -aruco_side_length/2, 0]
+])
+
 while True:
     _, frame = cap.read()
 
@@ -26,29 +34,20 @@ while True:
 
     corners, ids, rejected = detector.detectMarkers(frame)
 
-    if(len(corners) > 0):
-        ids = ids.flatten().astype(int)
-        for c,i in zip(corners, ids):
-            top_left, top_right, bottom_right, bottom_left = c.reshape((4,2)).astype(int)
+    if(ids is not None):
+        success, rvecs, tvecs = cv2.solvePnP(object_points, corners[0][0], new_camera_matrix, dist_coeff)
+        if success:
+            cv2.aruco.drawDetectedMarkers(frame, corners, ids, (0,255,0))
 
-            top_left = (top_left[0], top_left[1])
-            top_right = (top_right[0], top_right[1])
-            bottom_right = (bottom_right[0], bottom_right[1])
-            bottom_left = (bottom_left[0], bottom_left[1])
+            rotation_matrix, _ = cv2.Rodrigues(rvecs)
+            r = R.from_matrix(rotation_matrix)
+            quat = r.as_quat()
 
-            cv2.line(frame, top_left, top_right, (0,255,0), 2)
-            cv2.line(frame, top_right, bottom_right, (0,255,0), 2)
-            cv2.line(frame, bottom_right, bottom_left, (0,255,0), 2)
-            cv2.line(frame, bottom_left, top_left, (0,255,0), 2)
+            rotation_transform_x = quat[0]
+            rotation_transform_y = quat[1]
+            rotation_transform_z = quat[2]
+            rotation_transform_w = quat[3]
 
-            cx = (top_left[0] + bottom_right[0])//2
-            cy = (top_left[1] + bottom_right[1])//2
-
-            cv2.circle(frame, (cx,cy), 4, (0,0,255), -1)
-        
-        cv2.imshow("aruco", frame)
-        if cv2.waitKey(0) & 0xFF == ord('q'):
-            break
 
 cap.release()
 cv2.destroyAllWindows()
