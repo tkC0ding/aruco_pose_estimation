28efb8d53f80dadc43d62c9a5c4a26c4b5303e10
tkC0ding <tkjagani@gmail.com>
2024-10-02 11:29:42 +0530

    Implemented direct linear transform to find the initial projection matrix

--- SOLVEPNP.py
+++ SOLVEPNP.py
@@ -8,3 +8,18 @@ def normalize_point(camera_matrix, image_points):
     normalized_img_points = np.dot(K_inv, img_points.T).T
 
     return normalized_img_points[:, :2]
+
+def DLT(object_points, image_points):
+    n = object_points.shape[0]
+    A = []
+    for i in range(n):
+        X, Y, Z = object_points[i][0], object_points[i][1], object_points[i][2]
+        u, v = image_points[i][0], image_points[i][1]
+
+        A.append([-X, -Y, -Z, -1, 0, 0, 0, 0, u*X, u*Y, u*Z, u])
+        A.append([0, 0, 0, 0, -X, -Y, -Z, -1, v*X, v*Y, v*Z, v])
+    A = np.array(A)
+    U, sigma, Vt = np.linalg.svd(A)
+
+    P = np.reshape(Vt[-1], (3,4))
+    return P
